c35710e7009716d15bbb82602bc3e5a32444ad85
Valery Kobzar <kobzarvs@gmail.com>
2022-12-23 09:13:59 +0300

    refactoring

--- src/2022/day21/aoc_lexer.py
+++ src/2022/day21/aoc_lexer.py
@@ -4,10 +4,12 @@ from sly import Lexer
 class AocLexer(Lexer):
     # Set of token names. This is always required
     tokens = {NAME, NUMBER, PLUS, MINUS, MUL, DIVIDE, ASSIGN}
+    literals = {':', '+', '-', '*', '/'}
 
     # String containing ignored characters between tokens
     ignore = ' \t\n'
     ignore_comment_line = r'\#.*|\/\/.*'
+    ignore_newline = r'\n+'
 
     # Regular expression rules for tokens
     PLUS = r'\+'
@@ -16,19 +18,12 @@ class AocLexer(Lexer):
     DIVIDE = r'/'
     ASSIGN = r':'
 
-    literals = {':', '+', '-', '*', '/'}
-
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
     @_(r'\d+(\.\d+)?')
     def NUMBER(self, t):
         t.value = float(t.value)
         return t
-
-    # Define a rule so we can track line numbers
-    @_(r'\n+')
-    def ignore_newline(self, t):
-        self.lineno += len(t.value)
 
     # Error handling rule
     def error(self, t):
