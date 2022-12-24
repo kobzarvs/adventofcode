b758258b0b0399b04ce36809e2e5417bc578cc9c
Valery Kobzar <kobzarvs@gmail.com>
2022-12-24 08:09:04 +0300

    add anight style

--- src/2022/day21/anight_style.py
+++ src/2022/day21/anight_style.py
@@ -5,19 +5,8 @@ operators = {"+": add, "-": sub, "*": mul, "/": floordiv}
 reversed_ops = {'+': '-', '-': '+', '*': '/', '/': '*'}
 
 
-def handle_expr(name, expr):
-    left, op, right = expr
-    return {
-        'expr': (name, left, op, right),
-        'eval': lambda: operators[op](monkeys[left]['eval'](), monkeys[right]['eval']()),
-    }
-
-
-def handle_number(name, value):
-    return {
-        'expr': (name, int(value)),
-        'eval': lambda: int(value),
-    }
+def handle_expr(name, left, op, right, updated):
+    return operators[op](monkeys[left](), monkeys[right]())
 
 
 def load_data(filename):
@@ -26,47 +15,49 @@ def load_data(filename):
             line = line.rstrip()
             name, expr = line.split(': ')
             if expr.isnumeric():
-                yield name, handle_number(name, expr)
+                yield name, partial(lambda name, value: int(value), name, expr)
             else:
-                yield name, handle_expr(name, expr.split())
+                left, op, right = expr.split()
+                yield name, partial(handle_expr, name, left, op, right, False)
 
 
-def find_monkey(search, arr):
+def find_monkey_expr(search, arr):
     for value in arr.values():
-        if len(value['expr']) != 2 and (value['expr'][1] == search or value['expr'][3] == search):
-            return value
+        if len(value.args) == 2:
+            continue
+        name, left, op, right, updated = value.args
+        if not updated and search in [left, right]:
+            return name, left, op, right
     return None
 
 
 def reverse_expr(name, expr):
-    result, left, op, right = expr
+    id, left, op, right = expr
     new_expr = None
     if left == name:
-        new_expr = (name, result, reversed_ops[op], right)
+        new_expr = (id, reversed_ops[op], right)
     elif right == name:
         if op in ['+', '*']:
-            new_expr = (name, result, reversed_ops[op], left)
+            new_expr = (id, reversed_ops[op], left)
         else:
-            new_expr = (name, left, op, result)
+            new_expr = (left, op, id)
     return new_expr
 
 
 def part_2(search):
     while True:
-        monkey = find_monkey(search, monkeys)
+        monkey = find_monkey_expr(search, monkeys)
         if monkey is None:
             break
-        if monkey['expr'][0] == 'root':
-            branches = [monkey['expr'][1], monkey['expr'][3]]
+        if monkey[0] == 'root':
+            branches = [monkey[1], monkey[3]]
             branches.remove(search)
-            monkeys[search]['eval'] = partial(lambda key: monkeys[key]['eval'](), branches[0])
+            monkeys[search] = partial(lambda key: monkeys[key](), branches[0])
             break
-        result, left, op, right = reverse_expr(search, monkey['expr'])
-        monkeys[search]['eval'] = partial(
-            lambda left, op, right: op(monkeys[left]['eval'](), monkeys[right]['eval']()), left, operators[op],
-            right)
-        search = monkey['expr'][0]
-    return monkeys['humn']['eval']()
+        left, op, right = reverse_expr(search, monkey)
+        monkeys[search] = partial(handle_expr, search, left, op, right, True)
+        search = monkey[0]
+    return monkeys['humn']
 
 
 if __name__ == '__main__':
@@ -79,5 +70,5 @@ if __name__ == '__main__':
 
     monkeys = dict(load_data(filename))
 
-    print('Part I:', monkeys['root']['eval']())
-    print('Part II:', part_2('humn'))
+    print('Part I:', monkeys['root']())
+    print('Part II:', part_2('humn')())
