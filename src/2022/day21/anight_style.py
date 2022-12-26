5294e554facf270920ca291e2a2c1f701f7e3dc7
Valery Kobzar <kobzarvs@gmail.com>
2022-12-26 04:27:50 +0300

    typings

--- src/2022/day21/anight_style.py
+++ src/2022/day21/anight_style.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import re
 from dataclasses import dataclass
 from operator import add, sub, mul, floordiv
-from typing import Dict, ClassVar
+from types import SimpleNamespace
+from typing import Dict, ClassVar, Generator, Tuple, Optional
 
 operators = {"+": add, "-": sub, "*": mul, "/": floordiv}
 inv_ops = {'+': '-', '-': '+', '*': '/', '/': '*'}
@@ -18,6 +19,10 @@ class Mask:
 class Number:
     name: str
     value: int
+    pattern: ClassVar[str] = r'(?P<name>\w+): (?P<value>\d+)'
+
+    def __post_init__(self) -> None:
+        self.value = int(self.value)
 
 
 @dataclass
@@ -30,22 +35,22 @@ class Expr:
 
     context: ClassVar[Dict[str, Expr | Number]] = {}
     refs: ClassVar[Dict[str, str]] = {}
+    pattern: ClassVar[str] = r'(?P<name>\w+): (?P<left>\w+) (?P<op>[-+*/]) (?P<right>\w+)'
 
-    def __new__(cls, *args, **kwargs):
-        if 'ref' in kwargs:
-            cls.refs[args[1]] = args[0]
-            cls.refs[args[3]] = args[0]
-        return super().__new__(cls)
-
-    @property
-    def value(self):
-        return operators[self.op](Expr.context[self.left].value, Expr.context[self.right].value)
+    def __post_init__(self) -> None:
+        if self.ref:
+            Expr.refs[self.left] = self.name
+            Expr.refs[self.right] = self.name
 
     @property
-    def inv_op(self):
+    def value(self) -> int:
+        return operators[self.op](Expr.get(self.left).value, Expr.get(self.right).value)
+
+    @property
+    def inv_op(self) -> str:
         return inv_ops[self.op]
 
-    def invert(self, via):
+    def invert(self, via: str) -> Tuple[str, Expr]:
         target = Mask(via)
         new_expr = Expr(target.NAME, self.name, self.inv_op, self.right)
         match self:
@@ -57,39 +62,48 @@ class Expr:
         return self.name, new_expr
 
     @staticmethod
-    def get(name):
+    def get(name: str) -> Expr:
         return Expr.context[name]
 
     @staticmethod
-    def get_all_refs(name):
+    def get_all_refs(name) -> Generator[Expr, None, None]:
         while (name := Expr.refs[name]) and name in Expr.refs:
             yield Expr.get(name)
 
 
 @dataclass
-class RegexEqual(str):
+class RegexParser:
     string: str
-    match: re.Match | None = None
+    match: Optional[re.Match] = None
 
-    def __eq__(self, pattern):
-        self.match = re.search(pattern, self.string)
+    def __eq__(self, other: Expr | Number) -> bool:
+        self.match = re.search(other.pattern, self.string)
         return self.match is not None
 
-    def __getattr__(self, item):
-        return self.match[item]
+    def __getattr__(self, item: str) -> Optional[str]:
+        if self.match is not None:
+            return self.match[item]
+
+    @property
+    def values(self) -> Optional[Tuple[str, ...]]:
+        if self.match is not None:
+            return self.match.groups()
 
 
-def load_data(filename):
+Ast = SimpleNamespace(Number=Number, Expr=Expr)
+
+
+def load_data(filename: str) -> Generator[Tuple[str, Number | Expr], None, None]:
     with open(filename, 'r') as f:
         for line in f:
-            match RegexEqual(line.rstrip()):
-                case r'(?P<name>\w+): (?P<value>\d+)' as e:
-                    yield e.name, Number(e.name, int(e.value))
-                case r'(?P<name>\w+): (?P<left>\w+) (?P<op>[+*-/]) (?P<right>\w+)' as e:
-                    yield e.name, Expr(e.name, e.left, e.op, e.right, ref=True)
+            match data := RegexParser(line.rstrip()):
+                case Ast.Number:
+                    yield data.name, Number(*data.values)
+                case Ast.Expr:
+                    yield data.name, Expr(*data.values, ref=True)
 
 
-def part_2(target):
+def part_2(target: str) -> int:
     search = target
     for monkey in Expr.get_all_refs(target):
         search, _ = monkey.invert(via=search)
