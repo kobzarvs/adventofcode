5fd40a591a9f7e3ae950b13f5f2508099a4658b9
Valery Kobzar <kobzarvs@gmail.com>
2024-12-02 20:58:50 +0300

    2024-02: add noul implementation

--- src/2024/day-02/src/main.rs
+++ src/2024/day-02/src/main.rs
@@ -2,18 +2,19 @@ use std::env;
 use std::fs;
 use std::io::{self};
 use std::path::PathBuf;
+use itertools::Itertools;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
     let filename = args
         .get(1)
         .map(|s| PathBuf::from(s))
         .unwrap_or_else(|| PathBuf::from("test.txt"));
-
     println!("Input file: {:?}", filename);
-
     let input = fs::read_to_string(filename).expect("Unable to read file");
+
+    //-------------------------------------------------------------------------------//
+
     let reports = get_reports(&input);
 
     let safe_reports = reports
@@ -52,10 +53,9 @@ fn is_safe_report(line: &[i32], can_fix: bool) -> bool {
 
         if diff_dir != dir || diff_dist < 1 || diff_dist > 3 {
             if can_fix {
-                return line.iter().enumerate().any(|(index, _)| {
-                    let mut new_line = line.to_vec();
-                    new_line.remove(index);
-                    is_safe_report(&new_line, false)
+                return line.iter().combinations(line.len() - 1).any(|sub_report| {
+                    let combo_values = sub_report.iter().map(|&&x| x).collect_vec();
+                    is_safe_report(&combo_values, false)
                 });
             }
             return false;
