b679591d442c663adcb01558e06ff6eed2c6dfa3
Valery Kobzar <kobzarvs@gmail.com>
2024-12-07 13:16:47 +0300

    2024-07: wip

---
+++ src/2024/day-07/rust/src/main.rs
@@ -0,0 +1,93 @@
+use day_07::read_file;
+use itertools::Itertools;
+use regex::Regex;
+use std::cmp::{Ordering, PartialEq};
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = read_file();
+
+    let expressions = parse(&input)?;
+
+    let part_1 = solve_1(&expressions);
+    let part_2 = solve_2(&expressions);
+
+    println!("Part I: {:?}", part_1);
+    println!("Part II: {:?}", part_2);
+
+    Ok(())
+}
+
+fn parse(input: &str) -> Result<Vec<(i64, Vec<i64>)>, regex::Error> {
+    let re = Regex::new(r"(?<result>\d+): (?<numbers>[\d\s]+$)")?;
+
+    let mut expressions: Vec<(i64, Vec<i64>)> = Vec::new();
+
+    for line in input.lines() {
+        if re.is_match(line) {
+            let expr = re.captures(line).unwrap();
+            let result_str = expr.name("result").unwrap().as_str();
+            println!("result_str: {}", result_str);
+            let result: i64 = result_str.parse().unwrap();
+            let numbers: Vec<i64> = expr
+                .name("numbers")
+                .unwrap()
+                .as_str()
+                .split(" ")
+                .map(|it| it.parse::<i64>().unwrap())
+                .collect();
+
+            expressions.push((result, numbers));
+        }
+    }
+    println!("{:?}", expressions);
+
+    Ok(expressions)
+}
+
+fn solve_1(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
+    expressions
+        .iter()
+        .map(|(expected, numbers)| {
+            for counter in 0..2_i64.pow(numbers.len() as u32 - 1) {
+                let mut result = numbers[0];
+                for i in 1..numbers.len() {
+                    let is_bit = counter & 1 << (i - 1) != 0;
+                    if is_bit {
+                        result *= numbers[i];
+                    } else {
+                        result += numbers[i];
+                    }
+                }
+                if result == *expected {
+                    return result;
+                }
+            }
+            return 0;
+        })
+        .sum()
+}
+
+fn solve_2(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
+    expressions
+        .iter()
+        .map(|(expected, numbers)| {
+            for counter in 0..3_i64.pow(numbers.len() as u32 - 1) {
+                let mut result = numbers[0];
+                for i in 1..numbers.len() {
+                    let c = counter % 3;
+                    match c {
+                        0b00 => result *= numbers[i],
+                        0b01 => result += numbers[i],
+                        0b10 => result = format!("{}{}", result, numbers[i]).parse::<i64>().unwrap(),
+                        _ => continue,
+                    }
+                }
+                if result == *expected {
+                    return result;
+                }
+            }
+            return 0;
+        })
+        .sum()
+}
