f1f5d77a877f9d4ee6659386aee5fd96a18c5187
Valery Kobzar <kobzarvs@gmail.com>
2024-12-07 13:56:34 +0300

    2024-07: ready

--- src/2024/day-07/rust/src/main.rs
+++ src/2024/day-07/rust/src/main.rs
@@ -10,46 +10,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let expressions = parse(&input)?;
 
     let part_1 = solve_1(&expressions);
-    let part_2 = solve_2(&expressions);
-
     println!("Part I: {:?}", part_1);
+
+    let part_2 = solve_2(&expressions);
     println!("Part II: {:?}", part_2);
 
     Ok(())
 }
 
-fn parse(input: &str) -> Result<Vec<(i64, Vec<i64>)>, regex::Error> {
+fn parse(input: &str) -> Result<Vec<(u64, Vec<u64>)>, regex::Error> {
     let re = Regex::new(r"(?<result>\d+): (?<numbers>[\d\s]+$)")?;
 
-    let mut expressions: Vec<(i64, Vec<i64>)> = Vec::new();
+    let mut expressions: Vec<(u64, Vec<u64>)> = Vec::new();
 
     for line in input.lines() {
         if re.is_match(line) {
             let expr = re.captures(line).unwrap();
             let result_str = expr.name("result").unwrap().as_str();
-            println!("result_str: {}", result_str);
-            let result: i64 = result_str.parse().unwrap();
-            let numbers: Vec<i64> = expr
+            let result: u64 = result_str.parse().unwrap();
+            let numbers: Vec<u64> = expr
                 .name("numbers")
                 .unwrap()
                 .as_str()
                 .split(" ")
-                .map(|it| it.parse::<i64>().unwrap())
+                .map(|it| it.parse::<u64>().unwrap())
                 .collect();
 
             expressions.push((result, numbers));
         }
     }
-    println!("{:?}", expressions);
 
     Ok(expressions)
 }
 
-fn solve_1(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
+fn solve_1(expressions: &Vec<(u64, Vec<u64>)>) -> u64 {
     expressions
         .iter()
         .map(|(expected, numbers)| {
-            for counter in 0..2_i64.pow(numbers.len() as u32 - 1) {
+            for counter in 0..2_u64.pow(numbers.len() as u32 - 1) {
                 let mut result = numbers[0];
                 for i in 1..numbers.len() {
                     let bits = (counter & 1 << (i - 1)) >> (i - 1);
@@ -68,11 +66,11 @@ fn solve_1(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
         .sum()
 }
 
-fn solve_2(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
+fn solve_2(expressions: &Vec<(u64, Vec<u64>)>) -> u64 {
     expressions
         .iter()
         .map(|(expected, numbers)| {
-            for counter in 0..3_i64.pow(numbers.len() as u32 - 1) {
+            for counter in 0..4_u64.pow(numbers.len() as u32 - 1) {
                 if counter % 3 > 2 {
                     continue;
                 }
@@ -82,8 +80,8 @@ fn solve_2(expressions: &Vec<(i64, Vec<i64>)>) -> i64 {
                     match bits {
                         0b00 => result *= numbers[i],
                         0b01 => result += numbers[i],
-                        0b10 => result = format!("{}{}", result, numbers[i]).parse::<i64>().unwrap(),
-                        _ => continue,
+                        0b10 => result = format!("{}{}", result, numbers[i]).parse::<u64>().unwrap(),
+                        _ => continue
                     }
                 }
                 if result == *expected {
