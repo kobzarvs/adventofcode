8a3e750d26c4ff437a65e994a7db9166a137805b
Valery Kobzar <kobzarvs@gmail.com>
2024-12-08 16:05:31 +0300

    2024-01: refactoring

--- src/2024/day-01/src/main.rs
+++ src/2024/day-01/src/main.rs
@@ -1,76 +1,86 @@
 use std::collections::HashMap;
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::{PathBuf};
+use std::path::PathBuf;
+use std::{env, fs};
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
 
-    let filename = args.get(1)
-        .map(|s| PathBuf::from(s))
-        .unwrap_or_else(|| PathBuf::from("test.txt"));
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::fmt::init();
+    divan::main();
+
+
+    let filename = get_filename();
 
     println!("Input file: {:?}", filename);
 
-    let input = File::open(&filename)?;
-    let reader = io::BufReader::new(input);
+    let input = fs::read_to_string(&filename).unwrap();
 
-    // Векторы для хранения столбцов
-    let mut column1 = Vec::new();
-    let mut column2 = Vec::new();
-
-    for line in reader.lines() {
-        let line = line?;
-        let numbers: Vec<i32> = line
-            .split_whitespace()
-            .map(|num| num.parse::<i32>().unwrap())
-            .collect();
-
-        if numbers.len() == 2 {
-            column1.push(numbers[0]);
-            column2.push(numbers[1]);
-        }
-    }
-
-    // Сортируем колонки
+    let (mut column1, mut column2) = parse(input);
     column1.sort_unstable();
     column2.sort_unstable();
 
-    // Находим расстояния между соответствующими элементами
-    let distances: Vec<i32> = column1
-        .iter()
-        .zip(column2.iter())
-        .map(|(a, b)| (a - b).abs())
-        .collect();
-
-    // Суммируем расстояния
-    let total_distance: i32 = distances.iter().sum();
-
     // Part I
+    let total_distance = solve_1(&mut column1, &mut column2);
     println!("Part I (сумма расстояний): {}", total_distance);
 
+    // Part II
+    let sum_vector3 = solve_2(&mut column1, &mut column2);
+    println!("Part II (степень сходства): {}", sum_vector3);
+
+    Ok(())
+}
+
+fn solve_2(column1: &mut Vec<i32>, column2: &mut Vec<i32>) -> i32 {
     // Группируем второй вектор, подсчитывая количество каждого числа
     let mut count_map: HashMap<i32, usize> = HashMap::new();
-    for &num in &column2 {
-        *count_map.entry(num).or_insert(0) += 1;
+    for num in column2 {
+        *count_map.entry(*num).or_insert(0) += 1;
     }
 
     // Вектор с повторами из второго столбца
     let mut vector3 = Vec::new();
 
     // Проходим по первому столбцу
-    for &num in &column1 {
-        if let Some(&count)  = count_map.get(&num) {
-            vector3.push(num * count as i32);
+    for num in column1 {
+        if let Some(&count) = count_map.get(&num) {
+            vector3.push(*num * count as i32);
         }
     }
 
     // Суммируем значения степеней сходства
-    let sum_vector3: i32 = vector3.iter().sum();
+    vector3.iter().sum()
+}
 
-    // Part II
-    println!("Part II (степень сходства): {}", sum_vector3);
+fn solve_1(column1: &mut Vec<i32>, mut column2: &mut Vec<i32>) -> i32 {
+    // Находим расстояния между соответствующими элементами
+    let total_distance: i32 = column1
+        .iter()
+        .zip(column2.iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum();
 
-    Ok(())
+    total_distance
+}
+
+fn parse(input: String) -> (Vec<i32>, Vec<i32>) {
+    let mut column1 = vec![];
+    let mut column2 = vec![];
+
+    for line in input.lines() {
+        let mut numbers = line.split_whitespace();
+
+        column1.push(numbers.next().unwrap().parse::<i32>().unwrap());
+        column2.push(numbers.next().unwrap().parse::<i32>().unwrap());
+    }
+    (column1, column2)
+}
+
+pub fn get_filename() -> PathBuf {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = args
+        .get(1)
+        .map(|s| PathBuf::from(s))
+        .unwrap_or_else(|| PathBuf::from("test.txt"));
+    filename
 }
