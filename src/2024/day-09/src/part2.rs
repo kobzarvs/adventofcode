1ee0da381b8ab5fc7401fc44ca09473c4eb19ff2
Valery Kobzar <kobzarvs@gmail.com>
2024-12-09 22:54:57 +0300

    2024-09: optimizations

--- src/2024/day-09/src/part2.rs
+++ src/2024/day-09/src/part2.rs
@@ -1,20 +1,31 @@
-use std::cell::RefCell;
-use std::iter::zip;
-use std::rc::Rc;
+// use std::cell::RefCell;
+// use std::iter::zip;
+// use std::rc::Rc;
 use itertools::{Itertools};
-use rayon::prelude::IntoParallelRefIterator;
+// use rayon::prelude::IntoParallelRefIterator;
 // use rayon::prelude::*;
 // use tracing::{trace};
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-enum SectorType {
-    Free(usize),
-    File(usize, usize),
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct File {
+    id: usize,
+    start: usize,
+    size: usize,
 }
 
-fn parse(input: &str) -> Vec<SectorType> {
-    let mut index = 0;
-    let mut file_id = 0;
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Hole {
+    start: usize,
+    size: usize,
+    // files: Vec<File>,
+}
+
+fn parse(input: &str) -> (Vec<File>, Vec<Hole>) {
+    let mut pos = 0;
+    let mut id = 0;
+
+    let mut files: Vec<File> = vec![];
+    let mut holes: Vec<Hole> = vec![];
 
     input
         .chars()
@@ -22,121 +33,40 @@ fn parse(input: &str) -> Vec<SectorType> {
         .map(|c| c as usize)
         .chunks(2)
         .into_iter()
-        .flat_map(|mut chunk| {
-            let file_blocks = chunk.next().unwrap();
-            let free_blocks = chunk.next().unwrap_or(0);
+        .for_each(|mut chunk| {
+            let file_size = chunk.next().unwrap();
+            let hole_size = chunk.next().unwrap_or(0);
 
-            let mut file_vec: Vec<SectorType> = Vec::with_capacity(file_blocks);
-            let mut free_vec: Vec<SectorType> = Vec::with_capacity(free_blocks);
+            files.insert(0, File{id, start: pos, size: file_size});
+            pos += file_size;
+            holes.push(Hole{start: pos, size: hole_size});
+            pos += hole_size;
 
-            for _ in 0..file_blocks {
-                file_vec.push(SectorType::File(file_id, index));
-                index += 1;
-            }
+            id += 1;
+        });
 
-            for _ in 0..free_blocks {
-                free_vec.push(SectorType::Free(index));
-                index += 1;
-            }
-
-            file_vec.extend(free_vec);
-            file_id += 1;
-
-            file_vec
-        })
-        .collect()
+    (files, holes)
 }
 
 #[tracing::instrument(level = "trace")]
 pub fn process(input: &str) -> miette::Result<String> {
-    let disk_map: Vec<SectorType> = parse(input);
+    let (mut files, mut holes) = parse(input);
+    let mut result = 0;
 
-    let disk_map = Rc::new(RefCell::new(disk_map));
-
-    let cloned_disk_map = Rc::clone(&disk_map);
-    let disk_map_snapshot: Vec<_> = cloned_disk_map.borrow().iter().cloned().collect();
-
-    let reversed_files = disk_map_snapshot
-        .into_iter()
-        .filter(|it| matches!(it, SectorType::File(..)))
-        .rev()
-        .chunk_by(|file| {
-            match file {
-                SectorType::File(id, _) => Some(*id),
-                _ => None,
-            }
-        });
-
-    reversed_files
-        .into_iter()
-        .for_each(|(_file_id, file_chunks)| {
-            let file_chunks = file_chunks.collect::<Vec<_>>();
-            // trace!("file: {:?} chunks: {:?}", file_id.unwrap(), file_chunks);
-
-            let mut last_free_index: Option<SectorType> = None;
-            let mut free_chunk_id = 0;
-
-            // Обрабатываем свободные сектора
-            let free_sectors_snapshot: Vec<_> = cloned_disk_map
-                .borrow()
-                .iter()
-                .filter(|it| matches!(*it, SectorType::Free(..)))
-                .cloned()
-                .collect();
-
-            // println!("file: {} free: {:?}", file_id.unwrap(), free_sectors_snapshot);
-
-            free_sectors_snapshot
-                .iter()
-                .chunk_by(|&free| {
-                    let last_free = last_free_index.clone();
-                    last_free_index = Some(free.clone());
-                    match (last_free, free.clone()) {
-                        (None, SectorType::Free(_)) => free_chunk_id,
-                        (Some(SectorType::Free(last)), SectorType::Free(current)) if current.abs_diff(last) <= 1 => {
-                            free_chunk_id
-                        }
-                        _ => {
-                            free_chunk_id += 1;
-                            free_chunk_id
-                        }
-                    }
-                })
-                .into_iter()
-                .any(|(_free_chunk_id, free_chunks)| {
-                    let free_chunks = free_chunks.collect::<Vec<_>>();
-
-                    if file_chunks.len() <= free_chunks.len() {
-                        let mut borrowed_disk_map = cloned_disk_map.borrow_mut();
-
-                        zip(&file_chunks, &free_chunks).all(|pattern| {
-                            if let (SectorType::File(id, file_node), SectorType::Free(free_sector)) = pattern {
-                                if *file_node < *free_sector {
-                                    // println!(" - free: {:?} [{} < {}] - file: {:?} free: {:?}", free_chunk_id, *file_node, *free_sector, file_chunks, free_chunks);
-                                    return false;
-                                }
-                                borrowed_disk_map[*free_sector] = SectorType::File(*id, *free_sector);
-                                borrowed_disk_map[*file_node] = SectorType::Free(*file_node);
-                                // println!(" + free: {:?} [{} > {}] - file: {:?} free: {:?}", free_chunk_id, *file_node, *free_sector, file_chunks, free_chunks);
-                                return true;
-                            }
-                            // println!("- PATTERN ERROR -");
-                            false
-                        })
-                    } else {
-                        // println!(" - NO SPACE: {:?} - file: {:?} free: {:?}", free_chunk_id, file_chunks, free_chunks);
-                        false
-                    }
-                });
-        });
-
-    let result = Rc::clone(&disk_map)
-        .borrow()
-        .iter()
-        .fold(0, |acc, pattern| match pattern {
-            SectorType::File(id, index) => acc + id * index,
-            _ => acc,
-        });
+    for file in files.iter_mut() {
+        for hole in holes.iter_mut() {
+            // если дырка меньше файла - пропускаем
+            if hole.size < file.size || file.start <= hole.start { continue; }
+            // меняем местами дырку с файлом
+            (hole.start, file.start) = (hole.start + file.size, hole.start);
+            // корректируем размер дырки
+            hole.size -= file.size;
+            // hole.files.push(*file);
+        }
+        for i in 0..file.size {
+            result += file.id * (file.start + i);
+        }
+    }
 
     Ok(result.to_string())
 }
