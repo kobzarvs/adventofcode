f4b25df6e7083372266d7aad576ecfdc79c5aded
Valery Kobzar <kobzarvs@gmail.com>
2024-12-26 02:17:21 +0300

    2024-25: refactoring

--- src/2024/day-25/src/main.rs
+++ src/2024/day-25/src/main.rs
@@ -1,38 +1,28 @@
-use std::fs;
 use itertools::Itertools;
+use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("data.txt").unwrap();
-    let mut puzzle: Vec<u64> = vec![];
-    let mut piece: u64;
-    let mut lines = input.lines();
-
-    while let Some(line) = lines.next() {
-        match line {
-            "#####" | "....." => {
-                piece = if line.starts_with("#") { 0b11111 } else { 0 };
-                for subline in lines.by_ref() {
-                    if subline.is_empty() {
-                        break;
-                    }
-                    piece = subline.chars().fold(piece, |mut acc, c| {
-                        acc <<= 1;
-                        acc |= if c == '#' { 1 } else { 0 };
-                        acc
-                    });
-                }
-                puzzle.push(piece);
-            }
-            _ => {}
-        }
-    }
-
-    let result = puzzle
+    let result = fs::read_to_string("data.txt")
+        .unwrap()
+        .lines()
+        .chunks(8)
+        .into_iter()
+        .map(|chunk| {
+            chunk
+                .take(7)
+                .flat_map(|it| it.chars())
+                .fold(0, |mut acc, c| {
+                    acc <<= 1;
+                    acc |= if c == '#' { 1 } else { 0 };
+                    acc
+                })
+        })
+        .collect::<Vec<u64>>()
         .iter()
         .combinations(2)
         .map(|p| p[0] & p[1])
         .filter(|&it| it == 0)
         .count();
-
+    
     println!("Day 25: {result}");
 }
